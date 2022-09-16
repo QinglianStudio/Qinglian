5edbab97b17d39b1f8933a6ea3e02d238638706d
southorange1228 <15280970040@163.com>
2022-09-16 11:03:27 +0800

    feat(build): rust project support build

---
+++ packages/sql-parser/scripts/build.js
@@ -0,0 +1,13 @@
+const { execSync } = require("child_process");
+
+const { TARGET_PLATFORM } = process.env;
+const { platform } = process;
+
+execSync(
+  `cargo-cp-artifact -nc lib/${
+    TARGET_PLATFORM || platform
+  }.node -- cargo build --message-format=json-render-diagnostics --release`,
+  {
+    stdio: "inherit",
+  }
+);
