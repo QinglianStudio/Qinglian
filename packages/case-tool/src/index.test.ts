59321f56b5b3cb009a72c4507e2589816f41c891
southorange1228 <15280970040@163.com>
2022-10-01 09:56:01 +0800

    test: add func unit test for case-tool

--- packages/case-tool/src/index.test.ts
+++ packages/case-tool/src/index.test.ts
@@ -1,8 +1,35 @@
-import { isSnakeCase } from ".";
+import { isCamelCase, isKebabCase, isPascalCase, isSnakeCase } from ".";
 
 describe("Name case tool Test", () => {
   it("snake-case", () => {
-    const isSnakeCaseResult = isSnakeCase("a-b");
-    expect(isSnakeCaseResult).toBeFalsy();
+    const notSnakeCaseResult = isSnakeCase("a-b");
+    expect(notSnakeCaseResult).toBeFalsy();
+
+    const isSnakeCaseResult = isSnakeCase("a_b");
+    expect(isSnakeCaseResult).toBeTruthy();
+  });
+
+  it("camel-case", () => {
+    const notCamelCase = isCamelCase("ABC");
+    expect(notCamelCase).toBeFalsy();
+
+    const isCamelCaseResult = isCamelCase("aBcD");
+    expect(isCamelCaseResult).toBeTruthy();
+  });
+
+  it("kebab-case", () => {
+    const notKebabCase = isKebabCase("a_b");
+    expect(notKebabCase).toBeFalsy();
+
+    const isKebabCaseResult = isKebabCase("a-b-c");
+    expect(isKebabCaseResult).toBeTruthy();
+  });
+
+  it("pascal-case", () => {
+    const notPascalCase = isPascalCase("abc");
+    expect(notPascalCase).toBeFalsy();
+
+    const isPascalCaseResult = isPascalCase("AbcDef");
+    expect(isPascalCaseResult).toBeTruthy();
   });
 });
--- packages/case-tool/src/index.ts
+++ packages/case-tool/src/index.ts
@@ -1,15 +1,40 @@
+/**
+ * validate name is snake case
+ * @example a_b
+ * @param name 
+ * @returns 
+ */
 export const isSnakeCase = (name: string) => {
   return /^(([a-z0-9])_?)+$/.test(name);
 };
 
+
+/**
+ * validate name is camel case
+ * @example userName
+ * @param name 
+ * @returns 
+ */
 export const isCamelCase = (name: string) => {
   return /^([a-z]([A-Za-z0-9]{0,}))(([A-Z]([A-Za-z0-9]{0,})){0,})$/.test(name);
 };
 
+/**
+ * validate name is kebab case
+ * @example a-b
+ * @param name 
+ * @returns 
+ */
 export const isKebabCase = (name: string) => {
-  return /^(([a-z0-9])_?)+$/.test(name);
+  return /^(([a-z0-9])-?)+$/.test(name);
 };
 
+/**
+ * validate name is pascal case
+ * @example UserName
+ * @param name 
+ * @returns 
+ */
 export const isPascalCase = (name: string) => {
   return /^([A-Z]([A-Za-z0-9]{0,}))+$/.test(name);
 };
