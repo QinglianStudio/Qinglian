125616ccbb1a68551bf887b578b011d8d0e722c6
southorange1228 <southorange1228@foxmail.com>
2022-11-17 09:26:21 +0800

    feat(mock): support inner collection client method

---
+++ packages/mock/src/index.ts
@@ -0,0 +1,2 @@
+export * from "./utils/dataSet";
+export * from "./utils/req";
---
+++ packages/mock/src/utils/req.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+/**
+ * 采集数据 默认请求地址为`http://127.0.0.1:4005/cgi/api/v1/data-collection`
+ * 可自定义实现上报
+ * @param data
+ * @returns
+ */
+export const collectionData = async (data: Array<Array<any>>) => {
+  const instance = axios.create({
+    baseURL: "http://127.0.0.1:4005/cgi/api",
+  });
+  const resp = await instance.post("/v1/data-collection", {
+    Data: data,
+  });
+  return resp;
+};
