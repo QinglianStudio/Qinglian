5ba867d1d2e1e3d16591982b521cd6c5c7294e20
southorange1228 <southorange1228@foxmail.com>
2022-11-19 15:12:49 +0800

    refactor(cli): rename libs to cmd

--- packages/cli/src/release.ts
+++ packages/cli/src/release.ts
@@ -5,7 +5,7 @@ export const release = async (...args) => {
   const options = args[0];
   const { tag } = options || {};
   if (tag) {
-    execa("sh", [join(__dirname, "../libs/git_tag_release.sh")]).stdout.pipe(
+    execa("sh", [join(__dirname, "../cmd/git_tag_release.sh")]).stdout.pipe(
       process.stdout
     );
   }
