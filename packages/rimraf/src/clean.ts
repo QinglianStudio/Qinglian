7d58e9f81c0d994e96c32fbd94a55014b658363a
southorange0929 <southorange0929@foxmail.com>
2023-07-21 21:40:35 +0800

    feat: use utils to impl log info with rimraf

--- packages/rimraf/src/clean.ts
+++ packages/rimraf/src/clean.ts
@@ -1,14 +1,15 @@
 import { rimrafSync } from "rimraf";
+import { Log } from "@qinglian/utils";
 import * as glob from "glob";
 
 export const clean = (deletePath: string[]) => {
   const paths = deletePath.reduce((t, i) => {
     return [...t, ...glob.sync(i)];
   }, []);
-  console.log("🫣 Start to clean some folders or files...");
+  Log.info("🫣 Start to clean some folders or files...");
   paths.forEach((i) => {
     rimrafSync(i);
-    console.log(`rm -rf ${i}`);
+    Log.warn(`rm -rf ${i}`);
   });
-  console.log("🌟 All tasks finished!");
+  Log.success("🌟 All tasks finished!");
 };
