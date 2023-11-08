7834057c32314a182c953103ef21945638484080
southorange0929 <southorange0929@foxmail.com>
2023-11-08 15:31:05 +0800

    feat: use tsup

--- packages/case-tool/CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# @qinglian/case-tool
-
-## 0.0.1
-
-### Patch Changes
-
-- case tool init
---
+++ packages/release-tool/tsup.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["src/index.ts"],
+  target: "es5",
+  format: ["cjs", "esm"],
+  legacyOutput: true,
+  platform: "node",
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  dts: true,
+  minify: true,
+  shims: true,
+});
